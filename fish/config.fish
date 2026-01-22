c7f657f52f375fbd83f2407ec81336c4aba3d174
Basti Goldbuch <sebastian.goldbuch@icloud.com>
2026-01-22 21:08:12 +0100

    add set -x HOMEBREW_NO_EMOJI 1

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,7 @@
 set -g fish_greeting
 set -g PATH /opt/homebrew/bin $PATH
 set -x STARSHIP_CONFIG ~/.config/starship/starship.toml
+set -x HOMEBREW_NO_EMOJI 1
 
 alias ls="eza --all --icons"
 alias cd="z"
